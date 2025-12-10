53dbc6fe3fb2762fef685bd4c68c7e500ffe46fa
Beatriz Monteiro Vieira <beatrizmonteirovieira@outlook.com>
2025-12-10 12:07:21 -0300

    dependencias

---
+++ src/auth/entities/usuariologin.ts
@@ -0,0 +1,6 @@
+// Não cria uma entidade ou uma tabela no bd, mas auxilia para recerber as credenciais do usuario
+
+export class UsuarioLogin{
+    public usuario: string
+    public senha: string
+}
