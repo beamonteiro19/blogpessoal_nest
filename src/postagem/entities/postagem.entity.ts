907b8f8a9e9ea0ea25228ed3603bccf5e9a2c3a2
Beatriz Monteiro Vieira <beatrizmonteirovieira@outlook.com>
2025-12-17 12:57:14 -0300

    Deploy do Projeto Blog Pessoal

--- src/postagem/entities/postagem.entity.ts
+++ src/postagem/entities/postagem.entity.ts
@@ -10,29 +10,38 @@ import {
 } from 'typeorm';
 import { Tema } from '../../tema/entities/tema.entity';
 import { Usuario } from '../../usuario/entities/usuario.entity';
+import { ApiProperty } from '@nestjs/swagger';
 
 @Entity({ name: 'tb_postagens' })
 export class Postagem {
   @PrimaryGeneratedColumn()
+  @ApiProperty()
   id: number;
 
   @IsNotEmpty()
+  @ApiProperty()
   @Column({ length: 100, nullable: false })
   titulo: string;
 
   @IsNotEmpty()
+  @ApiProperty()
   @Column({ length: 1000, nullable: false })
   texto: string;
 
   @UpdateDateColumn()
+  @ApiProperty()
   data: Date;
 
+  @ApiProperty()
   @ManyToOne(() => Tema, (tema) => tema.postagem, {
     onDelete: 'CASCADE',
   })
+
+  @ApiProperty()
   @JoinColumn({ name: 'tema_id' })
   tema: Tema;
 
+  @ApiProperty()
   @ManyToOne(() => Usuario, (usuario) => usuario.postagem, {
     onDelete: 'CASCADE',
   })
